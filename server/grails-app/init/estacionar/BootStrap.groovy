edd7d9f406fed3b79fd06dbd0cb9442ae3895aae
Luciano trujillo <luciano.trujillo@innovid.com>
2022-07-25 21:24:32 -0300

    finished

--- server/grails-app/init/estacionar/BootStrap.groovy
+++ server/grails-app/init/estacionar/BootStrap.groovy
@@ -3,13 +3,8 @@ package estacionar
 class BootStrap {
 
     def init = { servletContext ->
-        // create a driver a save it in db
-         /*def driver = new Driver(dni: "42822231", name: "Luciano Trujillo", licensePlate: "AA 123 BB")
-         driver.save()
-         def driverPili = new Driver(dni: "43081901", name: "Pilar Gaddi", licensePlate: "NN 123 AA")
-         driverPili.save()
          def inspector = new Inspector(name: "jorge")
-         inspector.save()*/
+         inspector.save()
     }
     def destroy = {
     }
